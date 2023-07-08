f657d9933e5cc01c723fe6883091583ec152c6db
Victor Henrique <victxl@gmail.com>
2023-07-07 21:34:19 -0300

    sextou

--- siteDeVendas/src/main/java/com/victxl/siteDeVendas/controllers/SiteDeVendasController.java
+++ siteDeVendas/src/main/java/com/victxl/siteDeVendas/controllers/SiteDeVendasController.java
@@ -1,13 +1,18 @@
 package com.victxl.siteDeVendas.controllers;
 
 import com.victxl.siteDeVendas.models.Cliente;
+import com.victxl.siteDeVendas.models.Endereco;
 import com.victxl.siteDeVendas.models.Vendedor;
 import com.victxl.siteDeVendas.repository.*;
+
+import ch.qos.logback.core.model.Model;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -37,25 +42,38 @@ public class SiteDeVendasController {
         return "cadastroCliente";
     }
 
-    @RequestMapping (value = "/cadastroCliente",method = RequestMethod.POST)
-    public  String cadastroCliente(Cliente cliente){
+    @RequestMapping(value = "/cadastroCliente", method = RequestMethod.POST)
+    public String cadastroCliente(@ModelAttribute("cliente") Cliente cliente,
+                                  @ModelAttribute("endereco") Endereco endereco,
+                                  Model model) {
+        // Configurar a associação entre Cliente e Endereco
+        cliente.setEndereco(endereco);
+
+        
+        // Salvar o Endereco primeiro, se necessário
+        if (endereco.getIdEndereco() == null) {
+            er.save(endereco);
+        }
+
+        // Salvar o Cliente
         cr.save(cliente);
-        return "redirect:/";
+
+        return "redirect:/cadastroCliente";
     }
 
 
     @RequestMapping("/listarCliente")
     public ModelAndView listarCliente() {
+        ModelAndView mav = new ModelAndView("listarCliente");
 
-        ModelAndView mv = new ModelAndView("listarCliente");
+        Iterable<Cliente> cliente = cr.findAll();
+        mav.addObject("cliente", cliente); // Ajuste o nome do atributo para "clientes"
 
-        Iterable<Cliente> clientes = cr.findAll();
-        mv.addObject("cliente", clientes);
-
-        return mv;
+        return mav;
     }
 
-    @RequestMapping(value="/alterarCliente/{codigoCliente}", method=RequestMethod.POST)
+
+    @RequestMapping(value="/alterarCliente/{idCliente}", method=RequestMethod.POST)
     public String alterarCliente(@Validated Cliente cliente, BindingResult result, RedirectAttributes attributes) {
 
         cr.save(cliente);
@@ -81,6 +99,8 @@ public class SiteDeVendasController {
     public  String cadastroVendedor(){
         return "cadastroVendedor";
     }
+    
+    
 
     @RequestMapping(value = "/cadastroVendedor",method = RequestMethod.POST)
     public  String cadastroVendedor(Vendedor vendedor) {
